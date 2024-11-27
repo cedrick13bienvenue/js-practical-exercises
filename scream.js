811b54fde2f9143032955e2b5c8b1c9f6f1d9228
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-11-27 16:39:21 +0200

    scream.js

---
+++ scream.js
@@ -0,0 +1,13 @@
+// Write a function that takes two parameters: 
+// the first is a value to be repeated, and the second is a number representing how many times to repeat it.
+// The function returns the repeated value.
+
+const scream  = function(x,y){
+    let result=""
+    for(let i=0;i<y;i++){
+        result+=x
+    }
+    return result
+}
+
+console.log(scream("i",7))
