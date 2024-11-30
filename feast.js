3ba8b2bb68a01e0c3f3ced82c0c888201e9e13bf
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-11-30 15:31:54 +0200

    feast.js

--- feast.js
+++ feast.js
@@ -4,7 +4,8 @@ For example, the great blue heron is bringing garlic naan and the chickadee is b
 
 Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 
-Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string.
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. 
+beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string.
  They will not contain numerals.*/}
 
  function feast(beast, dish) {
