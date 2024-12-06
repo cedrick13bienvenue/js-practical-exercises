0a915de74563dfd04018598aa2c93951f5d53695
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-06 23:08:52 +0200

    feast.js

--- feast.js
+++ feast.js
@@ -25,3 +25,4 @@ beast and dish may contain hyphens and spaces, but these will not appear at the 
     function feast(beast, dish) {
         return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
     }
+    
