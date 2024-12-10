9e63a1b6c0b2f7b53a50d6c3a7d04dc5487e0f99
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-10 22:41:20 +0200

    scream.js

--- scream.js
+++ scream.js
@@ -2,6 +2,7 @@
 // the first is a value to be repeated, and the second is a number representing how many times to repeat it.
 // The function returns the repeated value.
 
+
 const scream  = function(x,y){
     let result=""
     for(let i=0;i<y;i++){
