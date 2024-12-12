01eaebd93d32cf4eba4fc0e747f707d00fd78c21
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-12 11:15:18 +0200

    printerErrors.js

--- printerErrors.js
+++ printerErrors.js
@@ -16,4 +16,13 @@ s="aaaxbbbbyyhwawiwjjjwwm"
 printer_error(s) => "8/22"
  */}
 
- 
+function printerError(s) {
+    // your code
+   let count=0
+   for(let i=0;i<s.length;i++){
+     if(s[i]>"m"){
+       count++
+     }
+   }
+  return `${count}/${s.length}`
+}
