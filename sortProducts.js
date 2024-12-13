0998f4429884e25cc8a8085eb08c0a3aef2e1312
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-13 23:17:25 +0200

    sortProducts.js

--- sortProducts.js
+++ sortProducts.js
@@ -2,10 +2,13 @@
 // You are given an array of product objects, each containing a name (string) and a price (number). 
 //Your task is to implement a function called sortProducts that sorts the products based on their prices in ascending order. 
 
+
 const sortProducts = function(products){
     return products.sort((x,y)=>
     x.price - y.price )
 }
+
+
 console.log(sortProducts([ { name: "Laptop", price: 1200 },
 { name: "Mouse", price: 25 },
 { name: "Keyboard", price: 45 },
