932685621bf00cfe4ee23cdb68a224f083c6c596
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-19 09:13:00 +0200

    alphabetPosition.js

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -10,3 +10,6 @@ Example
 Input = "The sunset sets at twelve o' clock."
 Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"*/}
 
+function alphabetPosition(text) {
+  return text;
+} 
