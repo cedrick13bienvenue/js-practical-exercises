b91376b8a1953e4276528c48a52b1f6e54ebfb53
Cedrick Bienvenue <cedrick4smith@gmail.com>
2024-12-27 09:33:24 +0200

    count +ve numbers and sum -ve numbers

--- count+VE&sum-VE.js
+++ count+VE&sum-VE.js
@@ -8,3 +8,18 @@ If the input is an empty array or is null, return an empty array.
 Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].*/}
 
+function countPositivesSumNegatives(input) {
+    // your code here
+    if(!input||input.length===0) return []
+    let x = 0
+    let y = 0
+    for(let num of input){
+        if(num>0){
+            x++
+        }else if(num<0){
+            y+=num
+        }
+    }
+    return [x,y]
+  }
+  console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
