21c8842da117a4b47f917125edfd2b2d64ed9bbd
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-01-01 19:28:48 +0200

    create counter

---
+++ createCounter.js
@@ -0,0 +1,13 @@
+{/*Counter
+Your task is to implement a JavaScript function called createCounter that creates a closure to keep track of a counter variable. 
+The createCounter function should return another function, which we'll call counterFn.
+
+Each time you invoke the counterFn function, it should increase the counter variable by 1 and return the updated counter value.
+
+Prototype
+Implement a simple inheritance chain using prototypes and demonstrate how properties/methods are inherited.
+Create a constructor function and add methods to its prototype. Then, create instances of the constructor and observe how they share the methods.
+Use the new keyword to create instances of a constructor function and initialize their properties.
+Build a custom object using Object.create and set its prototype to a predefined object. Demonstrate how inheritance works in this scenario.
+*/}
+
