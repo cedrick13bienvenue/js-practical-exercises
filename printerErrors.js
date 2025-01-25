449e29cda4a413f22bb3da9e503516289410f5ac
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-01-25 21:16:01 +0200

    printerError

--- printerErrors.js
+++ printerErrors.js
@@ -1,4 +1,3 @@
-
 {/*In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
 
 The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
@@ -32,4 +31,3 @@ function printerError(s){
     const result = s.split("").filter(char=>char>"m").length
     return `${result}/${s.length}`
 }
-
