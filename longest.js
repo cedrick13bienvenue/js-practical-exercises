1c491d7b59e43c635251e7f6ff9b51d3e3c4e35d
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-01-29 21:49:13 +0200

    longest.js

--- longest.js
+++ longest.js
@@ -1,3 +1,4 @@
+
 {/*Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), 
 the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
@@ -37,3 +38,4 @@ function longest(s1, s2) {
     }
     return Object.values(dup)
   }
+
