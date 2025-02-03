abfceed201ffdb1484480a4f46e4bee6678b6256
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-02-03 12:23:20 +0200

    getTotalX.js

--- getTotalX.js
+++ getTotalX.js
@@ -44,3 +44,4 @@ Explanation
 4, 8 and 16 divide evenly into 16, 32, 96.
 
 4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b. */
+
