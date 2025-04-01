08650adbc48c7b50f6244b5e383698207fc97564
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-01 17:57:04 +0200

    isPanagram.js

--- isPangram.js
+++ isPangram.js
@@ -4,10 +4,15 @@ For example, the sentence "The quick brown fox jumps over the lazy dog" is a pan
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */}
 
 function isPangram(string){
+
     let strng = string.toLowerCase()
+    
     let result = new Set()
+    
     for(let char of strng){
+    
         if(char>="a" && char<="z"){
+    
             result.add(char)
         }
     }
