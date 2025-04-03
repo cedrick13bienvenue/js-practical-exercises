b1f026a94e3dcdf7cc864471d05db63da9e1d99b
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-03 23:52:26 +0200

    JadenCase.js

--- JadenCase.js
+++ JadenCase.js
@@ -9,6 +9,8 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"*/}
 
  String.prototype.JadenCase = function(){
-    return this.split(" ").map( word=> word.charAt(0).toUpperCase() + word.slice(1))
+    return this.split(" ")
+    .map( word=> word.charAt(0)
+    .toUpperCase() + word.slice(1))
 }
 
