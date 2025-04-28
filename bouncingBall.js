8aec2a41ad0ffc598c64d10c828dd9d8feaee151
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-28 23:44:12 +0200

    Alternative method

--- bouncingBall.js
+++ bouncingBall.js
@@ -55,3 +55,15 @@ function bouncingBall(h,  bounce,  window) {
     return seenCount;
   }
   
+  // OR 
+  function bouncingBall(h, bounce, window) {
+    if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
+  
+    let seenCount = 1;
+    for (let height = h * bounce; height > window; height *= bounce) {
+      seenCount += 2;
+    }
+    
+    return seenCount;
+  }
+  
