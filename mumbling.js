0d39d0e9d15889fbe6bb6bcfb110d66d4526c248
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-30 23:53:27 +0200

    Using for...of loop

--- mumbling.js
+++ mumbling.js
@@ -1,4 +1,5 @@
-{/*This time no story, no theory. The examples below show you how to write function accum:
+{
+  /*This time no story, no theory. The examples below show you how to write function accum:
 
 Examples:
 accum("abcd") -> "A-Bb-Ccc-Dddd"
@@ -6,38 +7,47 @@ accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-*/}
+*/
+}
 
-function accum(s){
-
-    return split("")
-    .map((char,index)=>
-    char.toUpperCase()+char.toLowerCase().repeat(index))
-    .join("-")
-
+function accum(s) {
+  return split("")
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join("-");
 }
 
 // OR
 
-function accum(s){
-    
-    let result=""
-    
-    for(let i=0;i<s.length;i++){
-        let char=s[i]
-        let newChar=char.toUpperCase()
-        
-        for(let j=0;j<i;j++){
-            newChar+=char.toLowerCase()
-        
-        }
-        result+=newChar
-        
-        if(i<s.length-1){
-            result+="-"
-        
-        }
+function accum(s) {
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    let newChar = char.toUpperCase();
+
+    for (let j = 0; j < i; j++) {
+      newChar += char.toLowerCase();
     }
-    return result
+    result += newChar;
+
+    if (i < s.length - 1) {
+      result += "-";
+    }
+  }
+  return result;
 }
 
+// OR
+
+function accum(s) {
+  let result = [];
+  let index = 0;
+
+  for (let char of s) {
+    let repeated = char.toUpperCase() + char.toLowerCase().repeat(index);
+    result.push(repeated);
+    index++;
+  }
+
+  return result.join("-");
+}
