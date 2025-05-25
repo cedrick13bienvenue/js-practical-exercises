125f21bd65177591be9780f96db6707d639c444f
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-25 15:18:13 +0200

    lastNRevert.js

---
+++ lastNRevert.js
@@ -0,0 +1,25 @@
+/* 7.lastNRevert
+Write a function named lastNRevert that receives 2 parameters:
+
+a string named text
+a number - n
+and returns the last n characters of text but in reverse order.
+
+
+Example 1
+Input: 
+text = "T-shirts", n=4
+Output: 
+"stri"
+
+Explanation
+Last 4 characters are irts and their reversed order is stri. */
+
+function lastNRevert(text, n) {
+  if (n <= 0 || n > text.length) {
+    return "";
+  }
+
+  const lastNChars = text.slice(-n);
+  return lastNChars.split("").reverse().join("");
+}
