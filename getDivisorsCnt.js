99801d11339d4b8abd67d51e03743dda25dacaa3
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-08 23:06:24 +0200

    Optimized Using Square Root

--- getDivisorsCnt.js
+++ getDivisorsCnt.js
@@ -21,3 +21,16 @@ function getDivisorsCnt(n) {
   return count;
 }
 
+// Optimized Using Square Root
+
+function getDivisorsCnt(n) {
+  let count = 0;
+  const sqrt = Math.floor(Math.sqrt(n));
+  
+  for (let i = 1; i <= sqrt; i++) {
+    if (n % i === 0) {
+      count += (i === n / i) ? 1 : 2; // perfect square
+    }
+  }
+  return count;
+}
