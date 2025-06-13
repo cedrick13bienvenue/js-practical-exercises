18f1e881e1851dc2aac4ef97ad33f11f9339c342
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-13 23:57:34 +0200

    avoid mutating original product array during sort

--- sortProducts.js
+++ sortProducts.js
@@ -22,3 +22,7 @@ function sortProducts(products) {
   }
   return products.sort(compareByPrice);
 }
+// OR
+function sortProducts(products) {
+  return [...products].sort((a, b) => a.price - b.price);
+}
