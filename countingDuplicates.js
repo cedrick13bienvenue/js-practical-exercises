9a762b4f1cc76b64bc73b6968d97b4066ce29110
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-25 23:44:03 +0200

    feat: count case-insensitive duplicates using hashmap frequency count

--- countingDuplicates.js
+++ countingDuplicates.js
@@ -1,4 +1,5 @@
-{/*Count the number of Duplicates
+{
+  /*Count the number of Duplicates
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
 The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
@@ -9,17 +10,15 @@ Example
 "indivisibility" -> 1 # 'i' occurs six times
 "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
 "aA11" -> 2 # 'a' and '1'
-"ABBA" -> 2 # 'A' and 'B' each occur twice*/}
-
-function countingDuplicates(str){
-    str = str.toLowerCase()
-    let store = {
-
-    }
-    for(let char of str){
-        store[char] = (store[char] || 0)+1
-    }
-    const duplicates = Object.values(store).filter(x=>x>1)
-    return duplicates.length
+"ABBA" -> 2 # 'A' and 'B' each occur twice*/
 }
 
+function countingDuplicates(str) {
+  str = str.toLowerCase();
+  let store = {};
+  for (let char of str) {
+    store[char] = (store[char] || 0) + 1;
+  }
+  const duplicates = Object.values(store).filter((x) => x > 1);
+  return duplicates.length;
+}
