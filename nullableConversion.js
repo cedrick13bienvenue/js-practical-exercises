32f8535bd9062071207647c441fe8c875416667e
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-26 23:17:21 +0200

    refactor: use Object.entries and forEach for nullable conversion

--- nullableConversion.js
+++ nullableConversion.js
@@ -1,30 +1,36 @@
 // Nullable conversion
 // you are given an object with properties that may contain null or undefined values.
-// Your task is to implement a function called convertNullableValues that checks if any of the properties in the object contains null or undefined. 
+// Your task is to implement a function called convertNullableValues that checks if any of the properties in the object contains null or undefined.
 // If a property is null convert it to zero and if a property is undefined convert it to an empty string. Return the modified object.
 
-const convertNullableValues = function(object){
-
-    for(let key in object){
-        
-        if(object[key]===null){
-            object[key]=0
-        }else if(object[key]===undefined){
-            object[key]="";
-        }
+const convertNullableValues = function (object) {
+  for (let key in object) {
+    if (object[key] === null) {
+      object[key] = 0;
+    } else if (object[key] === undefined) {
+      object[key] = "";
     }
-    
-    return object
-}
+  }
+
+  return object;
+};
 
 const sampleObject = {
-    name: "Alice",
-    age: null,
-    email: undefined,
-    city: "Kigali",
+  name: "Alice",
+  age: null,
+  email: undefined,
+  city: "Kigali",
 };
 
 const updatedObject = convertNullableValues(sampleObject);
 console.log(updatedObject);
 
+// OR
 
+function convertNullableValues(obj) {
+  Object.entries(obj).forEach(([key, value]) => {
+    if (value === null) obj[key] = 0;
+    else if (value === undefined) obj[key] = "";
+  });
+  return obj;
+}
