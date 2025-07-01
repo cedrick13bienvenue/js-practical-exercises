cc069ee41e7008e8ff2be38d736425891ac4c262
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-01 23:02:47 +0200

    refactor: use num() factory function to simplify number definitions

--- calculatingFunctions.js
+++ calculatingFunctions.js
@@ -14,25 +14,86 @@ Division should be integer division. For example, this should return 2, not 2.66
 eight(dividedBy(three())); 
 */
 
-function zero(op) { return op ? op(0) : 0; }
-function one(op) { return op ? op(1) : 1; }
-function two(op) { return op ? op(2) : 2; }
-function three(op) { return op ? op(3) : 3; }
-function four(op) { return op ? op(4) : 4; }
-function five(op) { return op ? op(5) : 5; }
-function six(op) { return op ? op(6) : 6; }
-function seven(op) { return op ? op(7) : 7; }
-function eight(op) { return op ? op(8) : 8; }
-function nine(op) { return op ? op(9) : 9; }
+function zero(op) {
+  return op ? op(0) : 0;
+}
+function one(op) {
+  return op ? op(1) : 1;
+}
+function two(op) {
+  return op ? op(2) : 2;
+}
+function three(op) {
+  return op ? op(3) : 3;
+}
+function four(op) {
+  return op ? op(4) : 4;
+}
+function five(op) {
+  return op ? op(5) : 5;
+}
+function six(op) {
+  return op ? op(6) : 6;
+}
+function seven(op) {
+  return op ? op(7) : 7;
+}
+function eight(op) {
+  return op ? op(8) : 8;
+}
+function nine(op) {
+  return op ? op(9) : 9;
+}
 
-function plus(b) { return a => a + b; }
-function minus(b) { return a => a - b; }
-function times(b) { return a => a * b; }
-function dividedBy(b) { return a => Math.floor(a / b); }
+function plus(b) {
+  return (a) => a + b;
+}
+function minus(b) {
+  return (a) => a - b;
+}
+function times(b) {
+  return (a) => a * b;
+}
+function dividedBy(b) {
+  return (a) => Math.floor(a / b);
+}
 
 // Several example usage:
 
 console.log(seven(times(five()))); // 35
-console.log(four(plus(nine())));  // 13
+console.log(four(plus(nine()))); // 13
 console.log(eight(minus(three()))); // 5
 console.log(six(dividedBy(two()))); // 3
+
+// OR
+
+const numbers = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const num = (n) => (op) => op ? op(n) : n;
+
+const zero = num(0);
+const one = num(1);
+const two = num(2);
+const three = num(3);
+const four = num(4);
+const five = num(5);
+const six = num(6);
+const seven = num(7);
+const eight = num(8);
+const nine = num(9);
+
+const plus = (b) => (a) => a + b;
+const minus = (b) => (a) => a - b;
+const times = (b) => (a) => a * b;
+const dividedBy = (b) => (a) => Math.floor(a / b);
