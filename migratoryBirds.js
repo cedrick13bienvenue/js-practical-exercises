f4bb19d0b85b769b5022e6155a801a3720efe48b
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-02 23:54:48 +0200

    refactor: use Map and forEach for cleaner frequency counting in migratoryBirds

--- migratoryBirds.js
+++ migratoryBirds.js
@@ -1,4 +1,5 @@
-{/*Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. 
+{
+  /*Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. 
 If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
 
 Example
@@ -57,46 +58,68 @@ Type :
 Type : 
 Type : 
 Type : 
-Two types have a frequency of , and the lower of those is type .*/}
-
-function migratoryBirds(arr) {
-    // Write your code here
-  let store = {}
-    for(let char of arr){
-        store[char] = (store[char]||0)+1
-    }
-    let result = null
-    let frequency = 0
-    for(let x in store){
-        if(store[x]>frequency){
-            frequency = store[x]
-            result = x
-        }
-    }
-    return Number(result)
+Two types have a frequency of , and the lower of those is type .*/
 }
 
+function migratoryBirds(arr) {
+  // Write your code here
+  let store = {};
+  for (let char of arr) {
+    store[char] = (store[char] || 0) + 1;
+  }
+  let result = null;
+  let frequency = 0;
+  for (let x in store) {
+    if (store[x] > frequency) {
+      frequency = store[x];
+      result = x;
+    }
+  }
+  return Number(result);
+}
 
-// OR 
+// OR
 
 function migratoryBirds(arr) {
-    let store = {};
-    for (let char of arr) {
-        store[char] = (store[char] || 0) + 1;
+  let store = {};
+  for (let char of arr) {
+    store[char] = (store[char] || 0) + 1;
+  }
+
+  let result = null;
+  let frequency = 0;
+
+  for (let x in store) {
+    let birdId = Number(x);
+    if (store[x] > frequency) {
+      frequency = store[x];
+      result = birdId;
+    } else if (store[x] === frequency && birdId < result) {
+      result = birdId;
     }
+  }
 
-    let result = null;
-    let frequency = 0;
+  return result;
+}
 
-    for (let x in store) {
-        let birdId = Number(x);
-        if (store[x] > frequency) {
-            frequency = store[x];
-            result = birdId;
-        } else if (store[x] === frequency && birdId < result) {
-            result = birdId;
-        }
+// OR
+
+function migratoryBirds(arr) {
+  const countMap = new Map();
+
+  arr.forEach((bird) => {
+    countMap.set(bird, (countMap.get(bird) || 0) + 1);
+  });
+
+  let maxFreq = 0;
+  let result = Infinity;
+
+  for (let [bird, count] of countMap) {
+    if (count > maxFreq || (count === maxFreq && bird < result)) {
+      maxFreq = count;
+      result = bird;
     }
+  }
 
-    return result;
+  return result;
 }
