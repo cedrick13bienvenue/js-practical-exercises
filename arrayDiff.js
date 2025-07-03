d4b54182fc85a00f2ac524bc169d21b0d0f5b37c
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-03 23:24:13 +0200

    perf: optimize arrayDiff using Set for faster lookups

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,28 +1,36 @@
-{/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+{
+  /*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 It should remove all values from list a, which are present in list b keeping their order.
 
 arrayDiff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 
-arrayDiff([1,2,2,2,3],[2]) == [1,3]*/}
-
-function arrayDiff(a, b) {
-    return a.filter(x=>!b.includes(x))
+arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
 }
 
+function arrayDiff(a, b) {
+  return a.filter((x) => !b.includes(x));
+}
 
-// OR 
+// OR
 
 function arrayDiff(a, b) {
-    let result = [];
-  
-    for (let i = 0; i < a.length; i++) {
-      if (b.indexOf(a[i]) === -1) { // not found in b
-        result.push(a[i]);
-      }
+  let result = [];
+
+  for (let i = 0; i < a.length; i++) {
+    if (b.indexOf(a[i]) === -1) {
+      // not found in b
+      result.push(a[i]);
     }
-  
-    return result;
   }
-  
+
+  return result;
+}
+
+// OR
+
+function arrayDiff(a, b) {
+  const bSet = new Set(b);
+  return a.filter((x) => !bSet.has(x));
+}
