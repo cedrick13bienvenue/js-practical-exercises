024549093079d297512317269ea973a43faf1e70
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-05 23:08:08 +0200

    fix: correct one-pass neighbor swap to prevent redundant double swap and out-of-bounds error

--- bubble.js
+++ bubble.js
@@ -1,20 +1,14 @@
-// let arr = [2,1,7,4,8]
-[1,2,7,4,8]
-[a,b] = [b,a]
+// Bubble Sort
 
-const happy = function(arr){
-    for(let i=0;i<=arr.length;i++){
-      if(arr[i]>arr[i+1]){
-        let swap = arr[i]
-         arr[i]=arr[i+1]
-        arr[i+1]=swap
+const bubbleSort = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
-      [arr[i], arr[i+1]] = [arr[i+1], arr[i]]
     }
-return arr
-}
+  }
+  return arr;
+};
 
-console.log(happy([2,1,7,1,8]))
-
-
-
+console.log(bubbleSort([2, 1, 7, 1, 8])); // 👉 [1, 1, 2, 7, 8]
