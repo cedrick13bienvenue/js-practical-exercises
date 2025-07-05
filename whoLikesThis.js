232246d522102b503abe585cbdc5b6111c6e4f5c
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-05 23:06:01 +0200

    refactor: rewrite likes function using nested ternary operators for brevity

--- whoLikesThis.js
+++ whoLikesThis.js
@@ -1,4 +1,5 @@
-{/* You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
+{
+  /* You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
 We want to create the text that should be displayed next to such an item.
 
 Implement the function which takes an array containing the names of people that like an item. 
@@ -11,36 +12,52 @@ It must return the display text as shown in the examples:
 ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 
-*/}
+*/
+}
 
 function likes(names) {
-    if (names.length === 0) {
+  if (names.length === 0) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+// OR
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
       return "no one likes this";
-    } else if (names.length === 1) {
+    case 1:
       return `${names[0]} likes this`;
-    } else if (names.length === 2) {
+    case 2:
       return `${names[0]} and ${names[1]} like this`;
-    } else if (names.length === 3) {
+    case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    } else {
-      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-    }
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
   }
+}
 
-  // OR 
+// OR
 
-  function likes(names) {
-    switch (names.length) {
-      case 0:
-        return "no one likes this";
-      case 1:
-        return `${names[0]} likes this`;
-      case 2:
-        return `${names[0]} and ${names[1]} like this`;
-      case 3:
-        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-      default:
-        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
-    }
-  }
-  
+function likes(names) {
+  return names.length === 0
+    ? "no one likes this"
+    : names.length === 1
+    ? `${names[0]} likes this`
+    : names.length === 2
+    ? `${names[0]} and ${names[1]} like this`
+    : names.length === 3
+    ? `${names[0]}, ${names[1]} and ${names[2]} like this`
+    : `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+}
