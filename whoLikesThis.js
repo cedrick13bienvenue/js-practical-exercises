2d2caed7a3e4146ddd3ef8c5fe82ce1af8edb01b
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-05 23:06:44 +0200

    refactor: use array destructuring for clarity in likes function

--- whoLikesThis.js
+++ whoLikesThis.js
@@ -76,3 +76,21 @@ function likes(names) {
     `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
   );
 }
+
+// OR
+
+function likes(names) {
+  const [a, b, c] = names;
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${a} likes this`;
+    case 2:
+      return `${a} and ${b} like this`;
+    case 3:
+      return `${a}, ${b} and ${c} like this`;
+    default:
+      return `${a}, ${b} and ${names.length - 2} others like this`;
+  }
+}
