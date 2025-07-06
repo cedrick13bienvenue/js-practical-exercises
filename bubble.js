1a0d74c2de29dc0ab4efb5a089ac4180eb86e254
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-06 23:47:47 +0200

    feat: implement selection sort algorithm

--- bubble.js
+++ bubble.js
@@ -29,3 +29,20 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
+// OR
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+    }
+  }
+  return arr;
+}
