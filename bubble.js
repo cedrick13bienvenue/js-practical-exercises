d7355111e3fdd40419755b118d526e780a9503bc
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-06 23:47:26 +0200

    perf: optimize bubble sort with early exit condition

--- bubble.js
+++ bubble.js
@@ -12,3 +12,20 @@ const bubbleSort = function (arr) {
 };
 
 console.log(bubbleSort([2, 1, 7, 1, 8])); // 👉 [1, 1, 2, 7, 8]
+
+// OR
+
+function bubbleSort(arr) {
+  let swapped;
+  for (let i = 0; i < arr.length - 1; i++) {
+    swapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break; // Exit early if no swaps
+  }
+  return arr;
+}
