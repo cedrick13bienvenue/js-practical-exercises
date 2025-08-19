8c307115e4618bd016f986d0e13069af39ce6e45
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-08-19 23:53:54 +0200

    refactor: use Array.from for concise powersOfTwo implementation

--- powersOf2.js
+++ powersOf2.js
@@ -1,18 +1,23 @@
-{/*Complete the function that takes a non-negative integer n as input, 
+{
+  /*Complete the function that takes a non-negative integer n as input, 
 and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
 
 Examples
 n = 0  ==> [1]        # [2^0]
 n = 1  ==> [1, 2]     # [2^0, 2^1]
-n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2] */}
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2] */
+}
 
-function powersOfTwo(n){
-    let x = []
-    for(let i=0;i<=n;i++){
-      x.push(2**i)
-    }
-   return x
- }
- 
+function powersOfTwo(n) {
+  let x = [];
+  for (let i = 0; i <= n; i++) {
+    x.push(2 ** i);
+  }
+  return x;
+}
 
- 
+// OR
+
+function powersOfTwo(n) {
+  return Array.from({ length: n + 1 }, (_, i) => 2 ** i);
+}
