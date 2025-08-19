819654911429b3b9e3b985097514172fbd43b68f
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-08-19 23:56:10 +0200

    perf: use bitwise shift for faster powersOfTwo

--- powersOf2.js
+++ powersOf2.js
@@ -71,3 +71,13 @@ function powersOfTwo(n) {
   } while (i <= n);
   return result;
 }
+
+// OR
+
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(1 << i);
+  }
+  return result;
+}
